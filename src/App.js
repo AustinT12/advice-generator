43aee424ebac312a0b2820daad614fdbfe2187d0
AustinT12 <austintran85@gmail.com>
2022-10-29 15:29:39 -0400

    image imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import mobile from './images/pattern-divider-mobile.svg';
 import desktop from './images/pattern-divider-desktop.svg';
+import dice from './images/icon-dice.svg';
+import './App.css';
 
 function App() {
   return (
     <div className='container'>
-      <h1>
-        
-      </h1>
+      <h1></h1>
 
       <p></p>
 
-      <source media='(min-width: 768px)' srcSet= {desktop}></source>
+      <picture>
+        <source media='(min-width: 768px)' srcSet={desktop}></source>
+        <img src={mobile} alt='' />
+      </picture>
+
+      <div>
+        <button>
+          <img src={dice} alt='' />
+        </button>
+      </div>
     </div>
   );
 }
