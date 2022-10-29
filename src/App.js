c89dade966aeef67d7417daf61b09739703097b6
AustinT12 <austintran85@gmail.com>
2022-10-29 15:23:31 -0400

    moved the images file to the src file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import mobile from './images/pattern-divider-mobile.svg';
+import desktop from './images/pattern-divider-desktop.svg';
+
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
+      <h1>
+        
+      </h1>
 
+      <p></p>
+
+      <source media='(min-width: 768px)' srcSet= {desktop}></source>
     </div>
   );
 }
